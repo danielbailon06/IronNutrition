9af659d339e06e13d392be7d53e9c8f26c0e30f3
Daniel Bailón <dani.hd6@gmail.com>
2026-03-19 18:33:41 +0100

    Added NotFoundPage Style

--- src/Routes/NotFoundPage.jsx
+++ src/Routes/NotFoundPage.jsx
@@ -1,10 +1,14 @@
-
+import { Link } from "react-router-dom";
+import "./NotFoundPage.css";
 
 function NotFoundPage() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="notfound">
+      <h1>404</h1>
+      <h2>Page not found</h2>
+      <p>The page you are looking for doesn’t exist.</p>
+
+      <Link to="/">Back to dashboard</Link>
     </div>
   );
 }
